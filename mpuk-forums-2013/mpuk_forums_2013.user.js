716508df42d52a429964198d85791b6485b69555
Lloyd Pick <lloydpick@gmail.com>
2013-11-25 16:28:29 +0000

    nav goes back

--- mpuk-forums-2013/mpuk_forums_2013.user.js
+++ mpuk-forums-2013/mpuk_forums_2013.user.js
@@ -2,7 +2,7 @@
 // @name         MPUK Forums 2013
 // @description  Style changes
 // @include      http://forums.multiplay.com/*
-// @version      0.1
+// @version      0.2
 // ==/UserScript==
 
 function addGlobalStyle(css) {
@@ -17,7 +17,12 @@ function addGlobalStyle(css) {
 
 addGlobalStyle('body { background: none !important; }');
 addGlobalStyle('header { display: none !important; }');
-addGlobalStyle('.above_body { display: none !important; }');
+addGlobalStyle('.news-block { display: none !important; }');
+addGlobalStyle('.cta { display: none !important; }');
+addGlobalStyle('.above_body { margin-top: 10px !important; }');
+addGlobalStyle('.above_body #content-header { height: auto !important; }');
+addGlobalStyle('.above_body #content-header iframe { display: none !important; }');
+addGlobalStyle('.doc_header { min-height: 12px !important }');
 addGlobalStyle('#terms { display: none !important; }');
 addGlobalStyle('#sidebar_container { display: none !important; }');
-addGlobalStyle('#content { margin-right: 0 !important; }')
+addGlobalStyle('#content { margin-right: 0 !important; }');
